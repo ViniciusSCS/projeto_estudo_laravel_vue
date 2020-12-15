58cf3b3699c01723cbe761c061706d10df13d734
ViniciusSCS <vinicius.eng.comp92@gmail.com>
2020-12-14 22:35:08 -0300

    Ajuste e melhoria na tela de Pessoas

--- coreui/src/views/cadastro/Pessoa.vue
+++ coreui/src/views/cadastro/Pessoa.vue
@@ -3,17 +3,19 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header" style="text-align: center">
-                    <strong style="">Formulário de Cadastro</strong>
+                    <strong style="text-transform: uppercase">Formulário de Cadastro</strong>
                 </div>
                 <div class="card-body">
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="nome">Nome</label>
-                            <input type="name" v-model="nome" class="form-control" id="nome" placeholder="Nome">
+                            <input type="name" v-model="nome" class="form-control" id="nome" placeholder="Nome"
+                                   style="text-transform: uppercase">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="email">Email</label>
-                            <input type="email" v-model="email" class="form-control" id="email" placeholder="Email">
+                            <input type="email" v-model="email" class="form-control" id="email" placeholder="Email"
+                                   style="text-transform: uppercase">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="cpf">CPF</label>
@@ -28,14 +30,16 @@
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="endereco">Endereço</label>
-                            <input type="text" v-model="endereco" class="form-control" id="endereco"
+                            <input type="text" v-model="endereco" class="form-control" style="text-transform: uppercase"
+                                   id="endereco"
                                    placeholder="Endereço">
                         </div>
                         <div class="form-group col-md-2">
                             <label>
                                 Telefone
                             </label>
-                            <input type="text" class="form-control" id="telefone" v-model="telefone" v-mask="'(##) # ####-####'" placeholder="(00) 0 0000-0000">
+                            <input type="text" class="form-control" id="telefone" v-model="telefone"
+                                   v-mask="'(##) # ####-####'" placeholder="(00) 0 0000-0000">
 
                         </div>
                         <div class="form-group col-md-2">
@@ -45,7 +49,7 @@
                         <div class="form-group col-md-4">
                             <label for="complemento">Complemento</label>
                             <input type="text" v-model="complemento" class="form-control" id="complemento"
-                                   placeholder="Complemento">
+                                   placeholder="Complemento" style="text-transform: uppercase">
                         </div>
                     </div>
 
@@ -61,6 +65,7 @@
                                 id="sexo"
                                 v-model="sexoSelecionado"
                                 :options="sexo"
+                                style="text-transform: uppercase"
                             ></v-select>
                         </div>
                     </div>
@@ -84,9 +89,9 @@ export default {
 
     methods: {
         salvar: function () {
-            let self = this
+            var self = this
 
-            if (self.sexoSelecionado == null || self.cpf == null || self.numero == null ||
+            if (self.sexoSelecionado == null || self.cpf == null || self.telefone == null ||
                 self.nome == null || self.email == null || self.endereco == null || self.complemento == null ||
                 self.data_nascimento == null) {
 
@@ -101,13 +106,15 @@ export default {
                     cpf: self.cpf,
                     nome: self.nome,
                     email: self.email,
-                    telefone: self.telefone,
+                    numero: self.numero,
                     endereco: self.endereco,
+                    telefone: self.telefone,
                     sexo: self.sexoSelecionado,
                     complemento: self.complemento,
                     data_nascimento: self.data_nascimento
                 }
 
+                self.limpaForm();
 
                 console.log('SALVO... ', dados)
 
@@ -118,9 +125,27 @@ export default {
                     showConfirmButton: false,
                     timer: 1500
                 })
+
             }
+        },
+
+        limpaForm: function () {
+            var self = this
+
+            self.cpf = ''
+            self.nome = ''
+            self.email = ''
+            self.endereco = ''
+            self.complemento = ''
+            self.data_nascimento = ''
 
 
+            self.numero = null
+            self.telefone = null
+            self.sexo.label = null
+            self.sexo.value = null
+            self.verificaRadio = null
+            self.sexoSelecionado = null
         }
     },
 
@@ -131,16 +156,17 @@ export default {
                 {value: 2, label: 'Feminino'},
             ],
 
-
-            email: '',
-            nome: '',
             cpf: '',
-            data_nascimento: '',
+            nome: '',
+            email: '',
             endereco: '',
             complemento: '',
+            data_nascimento: '',
+
+            numero: null,
             telefone: null,
-            sexoSelecionado: null,
             verificaRadio: null,
+            sexoSelecionado: null,
         }
     },
 }
